cc0779e028bf62addff83823f1f6ddd798c786e0
Alison Torres <semvalice@gmail.com>
2025-12-27 17:46:21 +0300

    Remove __init__.py and update test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -4,9 +4,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    main_page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    main_page.open()                      # открываем страницу
+    main_page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)  # инициализируем Page Object для страницы логина  
+    login_page.should_be_login_page()        # выполняем метод страницы — переходим на страницу логина
+
+def test_should_be_login_url(browser): 
+    main_page = MainPage(browser, link)  
+    main_page.open()                      
+    main_page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)                      
+    login_page.should_be_login_url()
+    
+def test_should_be_login_form(browser):
+    main_page = MainPage(browser, link)  
+    main_page.open()                      
+    main_page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)                        
+    login_page.should_be_login_form()
+    
+def test_should_be_register_form(browser):
+    main_page = MainPage(browser, link)  
+    main_page.open()                      
+    main_page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)                       
+    login_page.should_be_register_form()
