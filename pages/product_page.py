396798b1ef5cdc9d06ca885974dca067caca9e9e
Alison Torres <semvalice@gmail.com>
2026-01-02 02:19:55 +0300

    added negative tests

--- pages/product_page.py
+++ pages/product_page.py
@@ -29,3 +29,12 @@ class ProductPage(BasePage):
 
     def solve_quiz_and_get_code(self):
         super().solve_quiz_and_get_code()
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+        "Success message is presented, but should not be"
+
+    def should_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+        "Success message is not disappeared, but should be"
+    
--- test_product_page.py
+++ test_product_page.py
@@ -1,31 +1,54 @@
 import pytest
 from pages.product_page import ProductPage
-import time
 
-product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+# product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
-urls = [
-    f"{product_base_link}/?promo=offer{no}"
-    if no != 7
-    else pytest.param(
-        f"{product_base_link}/?promo=offer{no}",
-        marks=pytest.mark.xfail(reason="Bug", strict=True)
-    )
-    for no in range(10)
-]
+# urls = [
+#     f"{product_base_link}/?promo=offer{no}"
+#     if no != 7
+#     else pytest.param(
+#         f"{product_base_link}/?promo=offer{no}",
+#         marks=pytest.mark.xfail(reason="Bug", strict=True))
+#     for no in range(10)]
 
-@pytest.mark.parametrize('urls', urls)
-def test_guest_can_add_product_to_basket(browser, urls):
-    product_page = ProductPage(browser, urls)
+# @pytest.mark.parametrize('urls', urls)
+# def test_guest_can_add_product_to_basket(browser, urls):
+#     product_page = ProductPage(browser, urls)
+#     product_page.open()
+
+#     product_page.should_be_add_to_basket_button()
+
+#     product_name = product_page.get_product_name()
+#     product_price = product_page.get_product_price()
+
+#     product_page.add_product_to_basket()
+#     product_page.solve_quiz_and_get_code()
+
+#     product_page.should_be_success_message()
+#     product_page.should_be_product_in_basket(product_name, product_price) 
+
+product_link  = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9" 
+
+@pytest.mark.xfail(reason="Bug", strict=True)
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, product_link)
     product_page.open()
-
-    product_page.should_be_add_to_basket_button()
-
-    product_name = product_page.get_product_name()
-    product_price = product_page.get_product_price()
-
     product_page.add_product_to_basket()
     product_page.solve_quiz_and_get_code()
 
-    product_page.should_be_success_message()
-    product_page.should_be_product_in_basket(product_name, product_price) 
+    product_page.should_not_be_success_message() 
+
+def test_guest_cant_see_success_message(browser):
+    product_page = ProductPage(browser, product_link)
+    product_page.open()
+
+    product_page.should_not_be_success_message() 
+
+@pytest.mark.xfail(reason="Bug", strict=True) 
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, product_link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.solve_quiz_and_get_code()
+
+    product_page.should_be_disappeared() 
