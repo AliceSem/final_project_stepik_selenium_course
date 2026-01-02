eb61ad5db72c8f0deae2d038c92efebb72470176
Alison Torres <semvalice@gmail.com>
2026-01-02 22:00:36 +0300

    added tests for basket page

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,16 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    main_page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    main_page.open()                      # открываем страницу
+    main_page = MainPage(browser, link)   
+    main_page.open()                      
     main_page.go_to_login_page()
 
-    login_page = LoginPage(browser, browser.current_url)  # инициализируем Page Object для страницы логина  
-    login_page.should_be_login_page()        # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)   
+    login_page.should_be_login_page()        
 
 def test_should_be_login_url(browser): 
     main_page = MainPage(browser, link)  
@@ -34,3 +35,12 @@ def test_should_be_register_form(browser):
 
     login_page = LoginPage(browser, browser.current_url)                       
     login_page.should_be_register_form()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    main_page = MainPage(browser, link)
+    main_page.open()
+    main_page.go_to_basket_page()
+
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket_message()
+    basket_page.should_not_be_items_in_basket()
